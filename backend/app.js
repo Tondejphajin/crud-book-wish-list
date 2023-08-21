80d00139a31a48c570210ca4d2661b1cf118550a
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-21 09:52:00 +0700

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 import express from "express";
 import books from "./routes/books.js";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use(books);
 
 app.listen(port, () => {
-  console.log("Connecte to the backend");
+  console.log("Connected to the backend");
 });
