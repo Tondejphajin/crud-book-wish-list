32e22e5985b4fdb12bdb0af095723dcf9cc8ab5c
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-21 09:53:19 +0700

    Implement React router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Books from "../routes/Books.jsx";
+import Add from "../routes/Add.jsx";
+import Update from "../routes/Update.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Books />,
+  },
+  {
+    path: "/add",
+    element: <Add />,
+  },
+  {
+    path: "/update",
+    element: <Update />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
