bfd44c76577d041f0b62241d6e81ae4a28e91ef6
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-25 23:58:02 +0700

    Implement useState

--- client/routes/Add.jsx
+++ client/routes/Add.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+
 const Add = () => {
+  const [book, setBook] = useState({
+    name: "",
+    description: "",
+    price: null,
+    cover: "",
+  });
+
   return (
     <div className="form">
       <form action="/" method="POST">
         <h1>Add New Book</h1>
-        <input type="text" placeholder="name"></input>
-        <input type="text" placeholder="description"></input>
-        <input type="text" placeholder="cover"></input>
-        <input type="number" placeholder="price"></input>
+        <input type="text" placeholder="name" name="name"></input>
+        <input type="text" placeholder="description" name="description"></input>
+        <input type="text" placeholder="cover" name="cover"></input>
+        <input type="number" placeholder="price" name="price"></input>
       </form>
     </div>
   );
