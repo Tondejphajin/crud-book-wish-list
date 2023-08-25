85e926ca728fbf2f931dc1ed9068a0f8f0c151ef
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-25 23:55:44 +0700

    Add form's header

--- client/routes/Add.jsx
+++ client/routes/Add.jsx
@@ -2,6 +2,7 @@ const Add = () => {
   return (
     <div className="form">
       <form action="/" method="POST">
+        <h1>Add New Book</h1>
         <input type="text" placeholder="name"></input>
         <input type="text" placeholder="description"></input>
         <input type="text" placeholder="cover"></input>
