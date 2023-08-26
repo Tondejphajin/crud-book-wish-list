354b8da3b78c43ddf9a7b9d42469160a9c0da75a
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-26 18:27:41 +0700

    Add initial table creation

--- backend/database.js
+++ backend/database.js
@@ -7,4 +7,22 @@ const connection = mysql.createConnection({
   database: "books",
 });
 
+const createTableQuery = `
+  CREATE TABLE books (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    cover VARCHAR(255),
+    price DECIMAL(10, 2)
+  );
+`;
+
+connection.query(createTableQuery, (err, results) => {
+  if (err) {
+    console.error("An error occurred: ", err);
+    return;
+  }
+  console.log("Table created:", results);
+});
+
 export default connection;
