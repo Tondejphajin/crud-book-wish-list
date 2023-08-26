5f1da4255155e0f7a5f256fad385e37e183d6e67
tindpj <106011455+Tondejphajin@users.noreply.github.com>
2023-08-26 08:55:43 +0700

    Refractor the code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Books from "../routes/Books.jsx";
+import Add from "../routes/Add.jsx";
+import Update from "../routes/Update.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Books />,
+  },
+  {
+    path: "/add",
+    element: <Add />,
+  },
+  {
+    path: "/update",
+    element: <Update />,
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,27 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Books from "../routes/Books.jsx";
-import Add from "../routes/Add.jsx";
-import Update from "../routes/Update.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Books />,
-  },
-  {
-    path: "/add",
-    element: <Add />,
-  },
-  {
-    path: "/update",
-    element: <Update />,
-  },
-]);
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
